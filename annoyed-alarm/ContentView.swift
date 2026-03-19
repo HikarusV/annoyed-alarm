cf788917e7769ec81cc2664dcac91dd70cfac766
Nazwa Sapta Pradana <nazwapradana.pc@gmail.com>
2026-03-19 15:35:36 +0700

    Initial Commit

---
+++ annoyed-alarm/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  annoyed-alarm
+//
+//  Created by Nazwa Sapta Pradana on 19/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ annoyed-alarm/annoyed_alarmApp.swift
@@ -0,0 +1,17 @@
+//
+//  annoyed_alarmApp.swift
+//  annoyed-alarm
+//
+//  Created by Nazwa Sapta Pradana on 19/03/26.
+//
+
+import SwiftUI
+
+@main
+struct annoyed_alarmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
